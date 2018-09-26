1c7cd79d2302f3ddb3086aa608d29ed6d3384c30
ramanacs09 <ramanacs09@yahoo.com>
2018-09-26 13:25:17 +0530

    Update config.py

--- upload_es_resources/config.py
+++ upload_es_resources/config.py
@@ -10,7 +10,7 @@ es_endpoint = "os.environ['aws_es_endpoint']"
 # common config params
 aws_private_key = os.environ['aws_private_key']
 bastion_user = 'centos'
-bastion_ip = "34.222.33.88"
+bastion_ip = os.environ['jump_server_ip']
 localhost = '127.0.0.1'
 aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
 aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
